208ee059839f6c30539dea9e1fd6c1b9fbd44eb5
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-31 08:26:14 -0600

    set states

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  API_URL,
+  API_KEY,
+  API_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE } from '../config';
 // Import Components
 import HeroImage from './elements/HeroImage';
 import Grid from './elements/Grid';
@@ -7,15 +13,24 @@ import MovieThumb from './elements/MovieThumb';
 import SearchBar from './elements/SearchBar';
 import Spinner from './elements/Spinner';
 
-const Home = () => (
-  <>
+const Home = () => {
+  const [state, setState] = useState({ movies: [] });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  console.log(state);
+
+  return (
+    <>
     <HeroImage />
     <SearchBar />
     <Grid />
     <MovieThumb />
     <Spinner />
     <LoadMoreBtn />
-  </>
-)
+    </>
+  )
+}
+
 
 export default Home;
